923a8cb727457cae979b9016107a300ec9aa990b
Emmanex01 <108570606+Emmanex01@users.noreply.github.com>
2022-09-11 19:38:34 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
- *  Return: always return 0
+ * Return: alway returns 0
+ *
  */
+
 int main(void)
 {
 	char a;
 
-	for (a = 'a'; a >= 'z'; a++)
+	for (a = 'a'; a <= 'z' ; a++)
 	{
 		if (a == 'q' || a == 'e')
 		{
