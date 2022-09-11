e5436a35bf8d57b7b021746175755726ba009708
Emmanex01 <Nwanosiketochukwu99@gmail.com>
2022-09-11 11:51:24 -0700

    New

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
