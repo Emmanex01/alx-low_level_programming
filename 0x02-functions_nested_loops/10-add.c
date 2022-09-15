83304d757ad10330b783894f20f464d364c46781
Emmanex01 <108570606+Emmanex01@users.noreply.github.com>
2022-09-15 02:37:39 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-yb
+#include "main.h"
+
+/**
+ * add - adding it up
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of both integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
