7e31224b8c24c78f8cca6711e1edc3b8ec78074d
Emmanex01 <108570606+Emmanex01@users.noreply.github.com>
2022-09-15 15:18:07 +0100

    Update main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_
+#define MAIN_
+
+int _isupper(int c);
+/**                                                                                                                                     
+ * isupper - checks for uppercase                                                                                                       
+ *                                                                                                                                      
+ * @c: integer to check                                                                                                                 
+ *                                                                                                                                      
+ * Return: 1 if true, 0 if false                                                                                                        
+ */
+ 
+ #endif
