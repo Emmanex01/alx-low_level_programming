5621e13abfb0e832834e428db01ebc92c72e111e
Emmanex01 <Nwanosiketochukwu99@gmail.com>
2022-09-14 18:07:41 -0700

    New

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - main entry
+ *
+ * @n: An integer
+ *
+ * Return: always return 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = n;
+	}
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	return (0);
+}
