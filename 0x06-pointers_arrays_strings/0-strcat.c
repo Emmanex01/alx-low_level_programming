dcc39c661f0038a6c52a696f4bab60c8a24a782d
Emmanex01 <Nwanosiketochukwu99@gmail.com>
2022-09-21 17:05:55 -0700

    New

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ *
+ * @dest: contains a string
+ * @src: contains a string
+ *
+ * Return: always return 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest += src;
+	return (0);
+}
