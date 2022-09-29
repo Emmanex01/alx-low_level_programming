8950ad32358c849c70ceea5bbb0cc2b27f09daf6
Emmanex01 <108570606+Emmanex01@users.noreply.github.com>
2022-09-29 16:49:44 +0100

    Update main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+void _puts_recursion(char *s);
+/**                                                                                                                                    
+ * _puts_recursion - prints a string                                                                                                   
+ *                                                                                                                                     
+ * @s: points to the strings address                                                                                                   
+ *                                                                                                                                     
+ * Return: always return 0                                                                                                             
+ */
+ 
+#endif
