d654082da523fec190df9747e68ccd4edd60fd09
Emmanex01 <Nwanosiketochukwu99@gmail.com>
2022-09-29 08:36:30 -0700

    New

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: points to the strings address
+ *
+ * Return: always return 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	return (0);
+}
