cf5f8b2373e34fbc06527adfc78e8ec9d0b83855
Emmanex01 <108570606+Emmanex01@users.noreply.github.com>
2022-09-29 21:10:00 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_
 
 void _putchar();
+
 void _puts_recursion(char *s);
 /**                                                                                                                                    
  * _puts_recursion - prints a string                                                                                                   
@@ -10,5 +11,21 @@ void _puts_recursion(char *s);
  *                                                                                                                                     
  * Return: always return 0                                                                                                             
  */
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+
+int is_palindrome(char *s);
+
+int wildcmp(char *s1, char *s2);
  
 #endif
