cee0a29a287126ae6f7fc687af3d62cb2cb97c95
Emmanex01 <Nwanosiketochukwu99@gmail.com>
2022-10-01 10:23:43 -0700

    Command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the program's name
+ * @argc: counts the number of arguments
+ * @argv: array of pointer to array of strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+	return (0);
+}
