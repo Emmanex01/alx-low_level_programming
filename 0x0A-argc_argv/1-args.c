eb5858e09b3dd545c072d0e4d9900d89691d6cfe
Emmanex01 <Nwanosiketochukwu99@gmail.com>
2022-10-05 15:56:33 -0700

    Working with Arg to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* main - prints number of arguments
+ * @argc: counts number of arguments
+ * @argv: contains different arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: counts number of arg
+ * @argv: arrray of strings
+ * @a: input number
+ * @b: input number
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int result;
+
+}
